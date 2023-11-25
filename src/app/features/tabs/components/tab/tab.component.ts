3cacd9ca10ca41b3ac88c2a056d0bcb11db585d9
Meloun <forejt@able.cz>
2023-11-25 19:33:23 +0100

    fix: removed not needed styles

--- src/app/features/tabs/components/tab/tab.component.ts
+++ src/app/features/tabs/components/tab/tab.component.ts
@@ -5,7 +5,6 @@ import { TabBodyDirective } from '../../directives/tab-body.directive';
 @Component({
     selector: 'app-tab',
     template: '', // template is required from angular but not for our implementation, where this component is used as container for tabs and template are added dynamically for full-customization purposes
-    styleUrls: ['./tab.component.scss']
 })
 export class TabComponent {
     @ContentChild(TabHeaderDirective) header: TabHeaderDirective;
